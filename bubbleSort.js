d241cc35f504602af29dae0a890456c4c45b7497
Psyche47 <16-32498-2@student.aiub.edu>
2021-03-05 15:09:08 +0600

    Bubble sort added

---
+++ bubbleSort.js
@@ -0,0 +1,26 @@
+/* Bubble Sort Algorithm:
+   Best Case Complexity: O(n)
+   Worst Case Complexity: O(n*n)
+*/
+function bubbleSort(arr) {
+  const arrLen = arr.length;
+  for (let i = arrLen; i > 0; i--) {
+    let noSwaps;
+    for (let j = 0; j < i - 1; j++) {
+      noSwaps = true;
+      console.log(arr, arr[j], arr[j + 1]);
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+        noSwaps = false;
+      }
+    }
+    console.log("Completed one pass.");
+    if (noSwaps) break;
+  }
+  return arr;
+}
+
+console.log(bubbleSort([23, 1, 2, 3, 3, 4, 5])); // -> [ 1, 2, 3, 3, 4, 5, 23 ]
+console.log(bubbleSort([23, 1, 2, 33, 43, 4, 5])); // -> [ 1, 2, 4, 5, 23, 33, 43 ]
